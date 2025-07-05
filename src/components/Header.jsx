48432f868763bcc25e4121a548580cc3ffb70e8a
HaDock404 <delescluse.g@gmail.com>
2025-07-05 11:22:45 +0200

    header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import '../styles/header.css';
 import { Link } from 'react-router-dom'; //obligatoire avec hashrouter
+import Logo from '../assets/logo.png'
 
 
 function Header() {
@@ -7,16 +8,11 @@ function Header() {
     return (
         <header>
             <nav className='header_nav'>
-                <Link to="https://www.erwin-labs.com/" className='home_logo'>
+                <Link to="/" className='home_logo'>
                     <div className='erwin_logo'>
-                        <img src="https://cdn.prod.website-files.com/62d56c0bde36c5e99acfb524/648a060a4e983940f8857fb1_Group%20289660.svg" alt="" />
+                        <img src={Logo} alt="" />
                     </div>
-                    
-                    <div className='home_logo_x'>X</div>
-
-                    <div className='fortnite_logo'>
-                         <img src="https://cms-assets.unrealengine.com/AVzjeqAbLRKi3W5jq0CAvz/cmb81xhry3tsj07mzscwq44nf" alt="" />
-                    </div>      
+                    <div className='home_logo_x'>Knowledge Base</div>     
                 </Link>
             </nav>
         </header>
